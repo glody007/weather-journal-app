e796b7fa530f2d2b27b610d5892648f5f264a7b5
glody007 <glodymbutwile@gmail.com>
2022-07-02 19:05:34 +0200

    feat: get weather data from zip code

--- website/app.js
+++ website/app.js
@@ -5,21 +5,52 @@ let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
-
+const KEY = '7f7da594e7943a81894f9523ee0c8411'
 
 /* Function called by event listener */
 const onGenerateClicked = () => {
-    postData('/add', {
-        temperature: 28,
-        date: newDate,
-        userResponse: ''
+    getLocationData()
+    .then((locationData) => {
+        const weatherData = getWeatherData(locationData.lat, locationData.lon)
+        return weatherData
+    })
+    .then((weatherData) => {
+        postData('/add', {
+            temperature: weatherData.main.temp,
+            date: newDate,
+            userResponse: ''
+        })
     })
     .then(() => {
-        getData()
+        return getData()
+    })
+    .then((userData) => {
+        // TO DO: Display data
+        console.log(userData)
     })
 }
 
-/* Function to GET Web API Data*/
+/* Function to GET Location Data*/
+const getLocationData = async () => {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid=${KEY}`)
+    try {
+        const locationData = await response.json()
+        return locationData
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+/* Function to GET Weather Data*/
+const getWeatherData = async (lat, lon) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`)
+    try {
+        const weatherData = await response.json()
+        return weatherData
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 /* Function to POST data */
 const postData = async (url = '', data = {}) => {
@@ -49,7 +80,7 @@ const getData = async () => {
     })
     try {
         const data = await response.json()
-        // TO DO: display data 
+        return data
     } catch (error) {
         console.log(error.data)
     }
